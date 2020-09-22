b1a7540f19f0054c9b8289221db9d1574129c13f
vbumazhenko <vbumazhenko@mail.ru>
2020-09-22 16:46:04 +0700

    Init commit

---
+++ src/test/java/com/provb/keyeventchecker/KeyEventCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provb.keyeventchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyEventCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
